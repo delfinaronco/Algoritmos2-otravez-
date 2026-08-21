8632ca4ab021a5402fdbfbb019b7cf7635d05c01
delfinaronco <142460374+delfinaronco@users.noreply.github.com>
2026-08-21 18:09:25 -0300

    Add files via upload

---
+++ Fecha.java
@@ -0,0 +1,70 @@
+package aed;
+
+public class Fecha {
+    private int dia;
+    private int mes;
+
+    public Fecha(int dia, int mes) {
+        this.dia = dia;
+        this.mes = mes;
+    }
+
+    public Fecha(Fecha fecha) {
+        dia = fecha.dia;
+        mes = fecha.mes;
+    }
+
+    public Integer dia() {
+        return this.dia;
+    }
+
+    public Integer mes() {
+        return this.mes;
+    }
+
+    public String toString() {
+        return this.dia + "/" + this.mes;
+    }
+
+    @Override
+    public boolean equals(Object otra) {
+
+        boolean otraEsNull = (otra == null);
+        if (otraEsNull){
+            return false;
+        }
+        
+        boolean claseDistinta = otro.getClass() != this.getClass();
+
+        if (claseDistinta){
+            return false;
+        }
+
+        Fecha otraFecha = (Fecha) otra;
+
+        return this.dia == otraFecha.dia && this.mes == otraFecha.mes;
+        
+    }
+
+    public void incrementarDia() {
+        if (!(this.dia == this.diasEnMes(this.mes))){
+            this.dia += 1;
+        } else {
+            this.dia = 1;
+            this.mes += 1;
+        }
+        if (this.mes > 12){
+            this.mes = 1;
+    }
+
+    private int diasEnMes(int mes) {
+        int dias[] = {
+                // ene, feb, mar, abr, may, jun
+                31, 28, 31, 30, 31, 30,
+                // jul, ago, sep, oct, nov, dic
+                31, 31, 30, 31, 30, 31
+        };
+        return dias[mes - 1];
+    }
+
+}
